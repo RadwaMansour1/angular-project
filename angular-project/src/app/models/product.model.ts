0c7e215ba95d3484d159feec29c64fc114c5cb62
Radwa Mansour <radwa.mansour2024@gmail.com>
2025-03-07 20:17:07 +0200

    Added category.model.ts and product.model.ts

---
+++ angular-project/src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  category:string;
+  rating: number;
+  price: number;
+  quantity:number;
+  imageUrl: string;
+  description: string;
+
+}
