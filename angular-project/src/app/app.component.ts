e889afe03af8a391ab57f31cc78b7e4f1742216e
Radwa Mansour <radwa.mansour2024@gmail.com>
2025-03-07 20:35:40 +0200

    Updated app.routes.ts and app.component.ts

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CartComponent } from './components/cart/cart.component';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CartComponent],
+  imports: [RouterOutlet, HttpClientModule,CategoryListComponent, FontAwesomeModule, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartComponent } from './components/cart/cart.component';
 import { PaymentComponent } from './components/payment/payment.component';
 
 export const routes: Routes = [
-    {path:'cart',component:CartComponent},
-    {path:'payment',component:PaymentComponent}
+  { path: 'products', component: CategoryListComponent },
+  { path: 'products/:category', component: ProductListComponent },
+  { path: 'products/:category/:productId', component: ProductDetailsComponent },
+  {path:'cart',component:CartComponent},
+  {path:'payment',component:PaymentComponent}
+
 ];
