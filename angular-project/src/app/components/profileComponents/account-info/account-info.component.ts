3262189dca9c80a5cca558f7dc13f6ab3afc3151
“Ahmed <ahmedmostafa4497@gmail.com>
2025-03-10 09:20:31 +0200

    account-info

--- angular-project/src/app/components/profileComponents/account-info/account-info.component.ts
+++ angular-project/src/app/components/profileComponents/account-info/account-info.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
 @Component({
   selector: 'app-account-info',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './account-info.component.html',
   styleUrl: './account-info.component.css'
 })
 export class AccountInfoComponent {
+  firstName:string = "Ahmed";
+  lastName:string = "Mostafa";
+  email:string = "ahmed44@gmail.com";
+  gender:string = "Male";
 
+  fnameChanged:boolean = false;
+  lnameChanged:boolean = false;
+  emailChanged:boolean = false;
+  genderChanged:boolean = false;
+  
+  onFnameChange(){
+    this.fnameChanged = true
+  }
+  onLnameChange(){
+    this.lnameChanged = true
+  }
+  onEmailChange(){
+    this.emailChanged = true
+  }
+  onGenderChange(){
+    this.genderChanged = true
+  }
 }
