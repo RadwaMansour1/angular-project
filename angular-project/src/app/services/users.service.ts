ef3c7dc49c1c1d5434dde03c4634abd2eeb9aa32
ReemAym <reem.ayman52002@gmail.com>
2025-03-16 03:26:20 +0200

    handle error

--- angular-project/src/app/services/users.service.ts
+++ angular-project/src/app/services/users.service.ts
@@ -45,7 +45,6 @@ export class UsersService {
     return this.http.delete<UserInterface>(`http://localhost:3000/users/${id}`)
   }
   
-<<<<<<< HEAD
   async removeItemFromCart(userId: string, productId: string): Promise<UserInterface> {
     try {
       // Fetch the user
@@ -71,9 +70,7 @@ export class UsersService {
     }
   }
   
-=======
   updateUser(id:string ,newData:UserInterface):Observable<UserInterface>{
     return this.http.put<UserInterface>(`http://localhost:3000/users/${id}`,newData)
   }
->>>>>>> 0d903f36e645ff82efe8053cc8761e03e54e4415
 }
