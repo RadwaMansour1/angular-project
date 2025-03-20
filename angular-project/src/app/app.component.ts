3043f472464ac027b8b956bdb29fcef7209de99f
Radwa Mansour <radwa.mansour2024@gmail.com>
2025-03-21 01:28:39 +0200

    Fixed error in SupportComponent

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -8,9 +8,11 @@ import { CartComponent } from './components/cart/cart.component';
 import { ProfileComponent } from "./components/profileComponents/profile/profile.component";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, CategoryListComponent, FontAwesomeModule,FavoriteComponent, CartComponent, ProfileComponent,NavbarComponent , FooterComponent],
+  imports: [RouterOutlet, HttpClientModule, CategoryListComponent, FontAwesomeModule,FavoriteComponent, CartComponent, ProfileComponent,NavbarComponent ,ReactiveFormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-project/src/app/components/support/support.component.ts
+++ angular-project/src/app/components/support/support.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-support',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css'
 })
 export class SupportComponent {
 
-  sendMsg(){
-    alert("Message sent!");
+
+  supportForm: FormGroup;
+  messageSent: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.supportForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phone: [''],
+      message: ['']
+    });
+  }
+
+  sendMsg() {
+    this.messageSent = "Thank you! Your message has been sent.";
+
+    this.supportForm.reset();
+
+    setTimeout(() => {
+      this.messageSent = '';
+    }, 3000);
   }
 }
