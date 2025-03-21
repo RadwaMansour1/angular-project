e30f152e9cc557642ddaa1aa2c45a948dbef9a4a
TadrosNasr <tadrosnasr7@gmail.com>
2025-03-21 14:16:48 +0200

    add some validations on support page inputs

--- angular-project/src/app/components/support/support.component.ts
+++ angular-project/src/app/components/support/support.component.ts
@@ -13,18 +13,27 @@ export class SupportComponent {
 
   supportForm: FormGroup;
   messageSent: string = '';
+  showError: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.supportForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      email: [''],
-      phone: [''],
-      message: ['']
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phone: ['', Validators.required, Validators.pattern('^[01][0-9]{9}$')],
+      message: ['', Validators.required]
     });
   }
 
   sendMsg() {
+    if (this.supportForm.invalid) {
+      this.showError = true;
+      this.supportForm.markAllAsTouched();
+
+      // this.messageSent= "All inputs are required.";
+      return;
+    }
+    this.showError = false;
     this.messageSent = "Thank you! Your message has been sent.";
 
     this.supportForm.reset();
